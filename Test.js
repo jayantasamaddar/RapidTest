063238845be037c8df0a85d501693864bb9dc68c
Jayanta Samaddar <jay.s@kollablifestyle.com>
2022-04-04 02:55:44 +0530

    commit

--- Test.js
+++ Test.js
@@ -1,33 +1,4 @@
 /****************************************************************************************************/
-/* Example Functions */
-/****************************************************************************************************/
-const stringify = array => console.log(dataArray.join(","));
-const bubbleSort = array => {
-    const arr = [...array];
-    for(let i = 0; i < arr.length - 1; i++) {
-        for(let j = 0; j < arr.length - 1 - i; j++) {
-            if(arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-        }
-    }
-    return arr;
-}
-const sum = array => ((a, b) => a + b, 0);
-
-
-/****************************************************************************************************/
-/* Testing Setup */
-/****************************************************************************************************/
-
-const dataArray = [ // Enter your data here,
-    [62, 15, 24, 82, 43, 52, 112, 74, 96, 101, 31, 123],
-    [72, 33, 15, 996, 741, 224, 362, 1201, 42, 112, 233, 112]
-];
-const testFunctions = [stringify, bubbleSort, sum]; //separate functions with commas
-
-
-/****************************************************************************************************/
 /* Test Function to execute every iteration of Data against each and every function */
 /****************************************************************************************************/
 
@@ -46,6 +17,37 @@ const Test = (dataArray, functionsArray) => {
     return recursiveTest(dataArray, 0);
 }
 
-Test(dataArray, testFunctions);
-
 module.exports = Test
+
+
+/****************************************************************************************************/
+/* Example Functions */
+/****************************************************************************************************/
+// const stringify = array => console.log(dataArray.join(","));
+// const bubbleSort = array => {
+//     const arr = [...array];
+//     for(let i = 0; i < arr.length - 1; i++) {
+//         for(let j = 0; j < arr.length - 1 - i; j++) {
+//             if(arr[j] > arr[j + 1]) {
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//             }
+//         }
+//     }
+//     return arr;
+// }
+// const sum = array => ((a, b) => a + b, 0);
+
+
+/****************************************************************************************************/
+/* Testing Setup */
+/****************************************************************************************************/
+
+// const dataArray = [ // Enter your data here,
+//     [62, 15, 24, 82, 43, 52, 112, 74, 96, 101, 31, 123],
+//     [72, 33, 15, 996, 741, 224, 362, 1201, 42, 112, 233, 112]
+// ];
+// const testFunctions = [stringify, bubbleSort, sum]; //separate functions with commas
+
+
+// Test(dataArray, testFunctions);
+
